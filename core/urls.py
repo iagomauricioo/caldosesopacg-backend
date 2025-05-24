3582dc98b6d5d7fcf4ef3bd770a31d3d87fa1001
joão mota <ejoao282@gmail.com>
2025-05-24 02:42:51 -0300

    correção na api de clients

--- core/urls.py
+++ core/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
         name="schema-swagger-ui",
     ),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
-    path("/api", include("clients.urls")),
+    path("api/", include("customers.urls")),
 ]
